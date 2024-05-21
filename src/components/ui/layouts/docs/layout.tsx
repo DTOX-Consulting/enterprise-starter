16f275282412117084133836bb51bfe12068fea8
Devonte <devo.tox.89@gmail.com>
2024-05-21 14:12:57 +0100

    docs

--- src/components/ui/layouts/docs/layout.tsx
+++ src/components/ui/layouts/docs/layout.tsx
@@ -1,27 +1,20 @@
-import { G } from '@mobily/ts-belt';
-
-import { Header } from '@/components/ui/layouts/dashboard/header';
 import { Main } from '@/components/ui/layouts/dashboard/main';
 import { Sidebar } from '@/components/ui/layouts/docs/sidebar';
 import { cn } from '@/lib/utils';
 import { ContainerAnimated } from '@/components/ui/atoms/container';
 
 interface LayoutProps {
-  className?: string;
-  sidebarWidth?: number;
   children: React.ReactNode;
 }
 
-export default function Layout({ className, sidebarWidth, children }: LayoutProps) {
+export default function Layout({ children }: LayoutProps) {
   return (
     <ContainerAnimated className="size-full flex mt-8 gap-8">
       <Sidebar />
 
       <section
         className={cn(
-          '-ml-96 flex max-h-screen w-full grow flex-col transition-all duration-150 ease-in md:ml-0',
-          G.isNotNullable(sidebarWidth) && `-ml-${sidebarWidth}`,
-          className
+          '-ml-96 flex max-h-screen w-full grow flex-col transition-all duration-150 ease-in md:ml-0'
         )}
       >
         <Main>{children}</Main>
