3f3ae97562f44bb8521b5f871f4b43144cf8f854
Devonte <devo.tox.89@gmail.com>
2024-10-17 08:48:17 +0100

    chore: update code

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,10 @@ import eslintExtreme from 'eslint-config-extreme';
 const config = [
   {
     ignores: [
+      '**/dist/**',
+      '**/build/**',
+      '**/coverage/**',
+      '**/node_modules/**',
       'src/iframe/',
       'storybook-static',
       'src/components/ui/organisms/chat/chat-message.tsx'
