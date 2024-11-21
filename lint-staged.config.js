87f9492f213fbc059b3d8aad274e1cea666ba292
Devonte <devo.tox.89@gmail.com>
2024-11-21 17:36:26 +0000

    feat: update ignores

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,6 @@
 export default {
   '*': [() => 'pnpm format'],
+  'package.json': [() => 'pnpm lint:package'],
   '*.{ts,tsx,js,jsx,json}': [
     () => 'pnpm typecheck',
     (/** @type {string[]} */ files) => `pnpm cross-env CI=true pnpm lint ${files.join(' ')}`
