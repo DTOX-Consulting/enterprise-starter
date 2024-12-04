bd1694125127fe6808ab550395aa9c7d3c12087a
Devonte <devo.tox.89@gmail.com>
2024-12-04 00:28:36 +0000

    feat: update

--- src/app/api/trigger/route.ts
+++ src/app/api/trigger/route.ts
@@ -2,7 +2,7 @@ import { createAppRoute } from '@trigger.dev/nextjs';
 
 import { client } from '@/lib/sdks/trigger/client';
 // eslint-disable-next-line import-x/no-unassigned-import
-import '@/trigger/jobs';
+import '@/trigger';
 
 // this route is used to send and receive data with Trigger.dev
 export const { POST } = createAppRoute(client);
--- src/trigger/jobs/index.ts
+++
@@ -1,5 +0,0 @@
-// export all your job files here
-
-export * from '@/trigger/jobs/auth';
-export * from '@/trigger/jobs/examples';
-export * from '@/trigger/jobs/subscription';
