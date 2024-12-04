91e08fad90022b0f7f0fa4b56bc71a1596bea457
Devonte <devo.tox.89@gmail.com>
2024-12-04 12:10:28 +0000

    fix: fixes

--- src/components/ui/molecules/mdx-components.tsx
+++ src/components/ui/molecules/mdx-components.tsx
@@ -5,16 +5,13 @@ import { MdxCard } from '@/components/ui/molecules/mdx-card';
 import { cn } from '@/lib/utils';
 
 import type { MDXComponents } from 'mdx/types';
-import type { ImgHTMLAttributes, HTMLAttributes } from 'react';
+import type { ImgHTMLAttributes, HTMLAttributes, PropsWithChildren } from 'react';
 
 const components: MDXComponents = {
-  h1: ({ className, ...props }: HTMLAttributes<HTMLHeadingElement>) => (
-    <h1
-      className={cn('mt-2 scroll-m-20 text-4xl font-bold tracking-tight', className)}
-      {...props}
-    />
+  h1: ({ className, ...props }: PropsWithChildren<React.HTMLAttributes<HTMLHeadingElement>>) => (
+    <h1 className={cn('mt-2 scroll-m-20 text-4xl font-bold', className)} {...props} />
   ),
-  h2: ({ className, ...props }: HTMLAttributes<HTMLHeadingElement>) => (
+  h2: ({ className, ...props }: PropsWithChildren<React.HTMLAttributes<HTMLHeadingElement>>) => (
     <h2
       className={cn(
         'mt-10 scroll-m-20 border-b pb-1 text-3xl font-semibold tracking-tight first:mt-0',
@@ -23,37 +20,41 @@ const components: MDXComponents = {
       {...props}
     />
   ),
-  h3: ({ className, ...props }: HTMLAttributes<HTMLHeadingElement>) => (
+  h3: ({ className, ...props }: PropsWithChildren<React.HTMLAttributes<HTMLHeadingElement>>) => (
     <h3
       className={cn('mt-8 scroll-m-20 text-2xl font-semibold tracking-tight', className)}
       {...props}
     />
   ),
-  h4: ({ className, ...props }: HTMLAttributes<HTMLHeadingElement>) => (
+  h4: ({ className, ...props }: PropsWithChildren<React.HTMLAttributes<HTMLHeadingElement>>) => (
     <h4
       className={cn('mt-8 scroll-m-20 text-xl font-semibold tracking-tight', className)}
       {...props}
     />
   ),
-  h5: ({ className, ...props }: HTMLAttributes<HTMLHeadingElement>) => (
+  h5: ({ className, ...props }: PropsWithChildren<React.HTMLAttributes<HTMLHeadingElement>>) => (
     <h5
       className={cn('mt-8 scroll-m-20 text-lg font-semibold tracking-tight', className)}
       {...props}
     />
   ),
-  h6: ({ className, ...props }: HTMLAttributes<HTMLHeadingElement>) => (
+  h6: ({ className, ...props }: PropsWithChildren<React.HTMLAttributes<HTMLHeadingElement>>) => (
     <h6
       className={cn('mt-8 scroll-m-20 text-base font-semibold tracking-tight', className)}
       {...props}
     />
   ),
-  a: ({ className, children, ...props }: HTMLAttributes<HTMLAnchorElement>) => (
+  a: ({
+    className,
+    children,
+    ...props
+  }: PropsWithChildren<React.AnchorHTMLAttributes<HTMLAnchorElement>>) => (
     <a className={cn('font-medium underline underline-offset-4', className)} {...props}>
       {children}
     </a>
   ),
   // eslint-disable-next-line id-length
-  p: ({ className, ...props }: HTMLAttributes<HTMLParagraphElement>) => (
+  p: ({ className, ...props }: PropsWithChildren<React.HTMLAttributes<HTMLParagraphElement>>) => (
     <p className={cn('leading-7 [&:not(:first-child)]:mt-6', className)} {...props} />
   ),
   ul: ({ className, ...props }: HTMLAttributes<HTMLUListElement>) => (
@@ -75,15 +76,21 @@ const components: MDXComponents = {
     <img className={cn('rounded-md border', className)} alt={alt} {...props} aria-label={alt} />
   ),
   hr: ({ ...props }) => <hr className="my-4 md:my-8" {...props} />,
-  table: ({ className, ...props }: HTMLAttributes<HTMLDivElement>) => (
+  table: ({
+    className,
+    ...props
+  }: PropsWithChildren<React.TableHTMLAttributes<HTMLTableElement>>) => (
     <div className="my-6 w-full overflow-y-auto">
-      <div className={cn('w-full grid', className)} role="grid" {...props} />
+      <table className={cn('w-full', className)} {...props} />
     </div>
   ),
-  tr: ({ className, ...props }: HTMLAttributes<HTMLTableRowElement>) => (
+  tr: ({ className, ...props }: PropsWithChildren<React.HTMLAttributes<HTMLTableRowElement>>) => (
     <tr className={cn('m-0 border-t p-0 even:bg-muted', className)} {...props} />
   ),
-  th: ({ className, ...props }: HTMLAttributes<HTMLTableCellElement>) => (
+  th: ({
+    className,
+    ...props
+  }: PropsWithChildren<React.ThHTMLAttributes<HTMLTableCellElement>>) => (
     <th
       className={cn(
         'border px-4 py-2 text-left font-bold [&[align=center]]:text-center [&[align=right]]:text-right',
@@ -92,7 +99,10 @@ const components: MDXComponents = {
       {...props}
     />
   ),
-  td: ({ className, ...props }: HTMLAttributes<HTMLTableCellElement>) => (
+  td: ({
+    className,
+    ...props
+  }: PropsWithChildren<React.TdHTMLAttributes<HTMLTableCellElement>>) => (
     <td
       className={cn(
         'border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right',
@@ -101,13 +111,13 @@ const components: MDXComponents = {
       {...props}
     />
   ),
-  pre: ({ className, ...props }: HTMLAttributes<HTMLPreElement>) => (
+  pre: ({ className, ...props }: PropsWithChildren<React.HTMLAttributes<HTMLPreElement>>) => (
     <pre
       className={cn('mb-4 mt-6 overflow-x-auto rounded-lg border bg-black py-4', className)}
       {...props}
     />
   ),
-  code: ({ className, ...props }: HTMLAttributes<HTMLElement>) => (
+  code: ({ className, ...props }: PropsWithChildren<React.HTMLAttributes<HTMLElement>>) => (
     <code
       className={cn('relative rounded border px-[0.3rem] py-[0.2rem] font-mono text-sm', className)}
       {...props}
