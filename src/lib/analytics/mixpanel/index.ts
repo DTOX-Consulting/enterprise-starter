ba87a0b2fe998e9e9c257e87d41d2ac2d7643b94
Devonte <devo.tox.89@gmail.com>
2025-01-03 19:48:47 +0000

    feat: faqs

--- src/lib/analytics/mixpanel/index.ts
+++ src/lib/analytics/mixpanel/index.ts
@@ -1,12 +1,10 @@
+/* eslint-disable import-x/no-named-as-default-member */
 import mixpanel from 'mixpanel-browser';
 
 import { isLocalOrExternal } from '@/lib/utils/dom';
 
 import type { AnalyticsEvent, EventProperties } from '@/lib/analytics/events';
 import type { AnalyticsService, AnalyticsConfig } from '@/lib/analytics/manager';
-
-// eslint-disable-next-line import-x/no-named-as-default-member
-const { init, track, reset, people, identify, opt_in_tracking, opt_out_tracking } = mixpanel;
 
 const knownSpecialProperties = new Set([
   'email',
@@ -29,8 +27,8 @@ export class MixpanelAnalytics implements AnalyticsService {
   init(): void {
     if (this.isOptedOut) return;
     const { token, debug, ...options } = this.config;
-    init(token, { debug, ...options });
-    track('app:loaded');
+    mixpanel.init(token, { debug, ...options });
+    mixpanel.track('app:loaded');
   }
 
   trackEvent(event: AnalyticsEvent, properties?: EventProperties): void {
@@ -43,12 +41,12 @@ export class MixpanelAnalytics implements AnalyticsService {
       feature: event.feature
     };
 
-    track(eventName, fullProperties);
+    mixpanel.track(eventName, fullProperties);
   }
 
   identify(userId: string, traits?: EventProperties): void {
     if (this.isOptedOut) return;
-    identify(userId);
+    mixpanel.identify(userId);
     if (traits) this.setUserProperties(traits);
   }
 
@@ -64,20 +62,22 @@ export class MixpanelAnalytics implements AnalyticsService {
       }
     }
 
-    people.set(processedProperties);
+    mixpanel.people.set(processedProperties);
   }
 
   optOut(): void {
     this.isOptedOut = true;
-    opt_out_tracking();
+    mixpanel.opt_out_tracking();
   }
 
   optIn(): void {
     this.isOptedOut = false;
-    opt_in_tracking();
+    mixpanel.opt_in_tracking();
   }
 
   reset(): void {
-    reset();
+    mixpanel.reset();
   }
 }
+
+/* eslint-enable import-x/no-named-as-default-member */
