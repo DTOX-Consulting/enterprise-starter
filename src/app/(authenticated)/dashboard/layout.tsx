12d038a7baeffa864c54a3debc2cc3a80a3e326e
Devonte <devo.tox.89@gmail.com>
2025-01-08 18:34:14 +0000

    feat: security

--- src/app/(authenticated)/dashboard/layout.tsx
+++ src/app/(authenticated)/dashboard/layout.tsx
@@ -1,5 +1,19 @@
-import Layout from '@/components/ui/layouts/dashboard/layout';
+import DashboardLayout from '@/components/ui/layouts/dashboard/layout';
 
 export const runtime = 'edge';
 
-export default Layout;
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+const headerProps = () => ({
+  showUser: false,
+  showShare: true,
+  showThemeToggle: false,
+  showNotifications: true,
+  showSwitcherNavigation: true
+});
+
+export default function Layout({ children }: LayoutProps) {
+  return <DashboardLayout headerProps={headerProps()}>{children}</DashboardLayout>;
+}
